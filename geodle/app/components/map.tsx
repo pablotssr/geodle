9e90fc5213daf2aef583b84ed840eb968cb1d3fa
Martincttrll <95213604+Martincttrll@users.noreply.github.com>
2024-05-29 13:58:42 +0200

    martinzinzin

--- geodle/app/components/map.tsx
+++ geodle/app/components/map.tsx
@@ -1,16 +1,16 @@
 // src/components/Map.tsx
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import { LatLngTuple } from "leaflet";
 
-export default function MyMap(props: any) {
-    const position: LatLngTuple = [46.603354, 1.888334];
-    const zoom = 6; // Adjust zoom level as needed
+export default function MyMap({ position, zoom }: any) {
+  // const position: LatLngTuple = [46.603354, 1.888334];
+  // const zoom = 6; // Adjust zoom level as needed
 
   return (
-    <MapContainer center={position} zoom={zoom} scrollWheelZoom={true} >
+    <MapContainer center={position} zoom={zoom} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- geodle/app/page.tsx
+++ geodle/app/page.tsx
@@ -1,18 +1,21 @@
-'use client';
-import dynamic from 'next/dynamic.js';
-import { useMemo
+"use client";
+import dynamic from "next/dynamic.js";
+import { useMemo } from "react";
+export default function MyPage() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("./components/map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
 
- } from 'react';
-  export default function MyPage() {
-  const Map = useMemo(() => dynamic(
-    () => import('./components/map'),
-    { 
-      loading: () => <p>A map is loading</p>,
-      ssr: false
-    }
-  ), [])
+  const position = [46.603354, 1.888334];
 
-  return <div>
-    <Map  />
-  </div>
+  return (
+    <div>
+      <Map position={position} zoom="6" />
+    </div>
+  );
 }
