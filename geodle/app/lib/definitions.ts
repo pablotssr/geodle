77cfd2c38d11e68cf9c5fc6384982090af8aaf47
pablotssr <113898912+pablotssr@users.noreply.github.com>
2024-06-05 10:56:31 +0200

    Update definitions.ts

--- geodle/app/lib/definitions.ts
+++ geodle/app/lib/definitions.ts
@@ -66,32 +66,14 @@ export interface City {
   city_code: string;
   department_number: string;
 }
-export enum GameStates {
-	"playing",
-	"win",
-	"lose",
-}
-export enum Statuses {
-	"absent",
-	"present",
-	"correct",
-	"guessing",
-}
 
-export type CharStatus = keyof typeof Statuses;
 
 interface Cell {
 	value: string;
 	status: CharStatus;
 }
 
-export type Row = Cell[];
 
-export type User = {
-	id: string;
-	solved: number;
-	name: string;
-};
 
 export interface City {
 	zip_code: string;
