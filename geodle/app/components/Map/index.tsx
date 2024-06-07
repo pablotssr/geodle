c352124a0be2a99df2a0155b4739544edf477a19
Félix SCHERER <felix.scherer@etu.umontpellier.fr>
2024-06-07 14:59:49 +0200

    fix: map bounds

--- geodle/app/components/Map/index.tsx
+++ geodle/app/components/Map/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import React, { useEffect, useState } from "react";
 import { MyMapProps } from "../../lib/definitions";
+import { LatLngBounds } from "leaflet";
 
 // Dynamic import for react-leaflet components to avoid SSR issues
 const MapContainer = dynamic(
@@ -27,7 +28,13 @@ if (typeof window !== "undefined") {
 }
 
 const defaultPosition: [number, number] = [46.603354, 1.888334];
-const defaultZoom = 6; // Adjust zoom level as needed
+const defaultZoom = 5; // Adjust zoom level as needed
+
+// Define the bounds for France
+const franceBounds = new LatLngBounds(
+	[41.3337, -5.5591], // Southwest coordinates (min latitude, min longitude)
+	[51.124, 9.6625] // Northeast coordinates (max latitude, max longitude)
+);
 
 const Map: React.FC<MyMapProps> = ({
 	position = defaultPosition,
@@ -56,16 +63,21 @@ const Map: React.FC<MyMapProps> = ({
 			className="mt-9 rounded"
 			center={position}
 			zoom={zoom}
+			maxZoom={8}
+			minZoom={5}
 			scrollWheelZoom={true}
+			maxBounds={franceBounds}
+			maxBoundsViscosity={1.0} 
 		>
-			{/* Utilize the dynamically selected tile layer URL */}
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url={tileLayerUrl}
 			/>
 			{markers.map((marker, index) => (
 				<Marker key={index} position={marker.position} icon={marker.icon}>
-					<Popup>{`${marker.nom_commune}`}</Popup>
+					<Popup>
+						{<div className="text-center font-bold">{marker.nom_commune}</div>}
+					</Popup>
 				</Marker>
 			))}
 		</MapContainer>
--- geodle/app/context/CityDataContext.tsx
+++ geodle/app/context/CityDataContext.tsx
@@ -1,87 +1,107 @@
-'use client';
+"use client";
 import { createContext, useContext, useState, useEffect } from "react";
-import { City, CityAdditionalData, CityDataContextType, CityDataProviderProps } from "../lib/definitions";
+import {
+	City,
+	CityAdditionalData,
+	CityDataContextType,
+	CityDataProviderProps,
+} from "../lib/definitions";
 
-const CityDataContext = createContext<CityDataContextType | undefined>(undefined);
+const CityDataContext = createContext<CityDataContextType | undefined>(
+	undefined
+);
 
 export const useCityData = (): CityDataContextType => {
-  const context = useContext(CityDataContext);
-  if (!context) {
-    throw new Error("useCityData must be used within a CityDataProvider");
-  }
-  return context;
+	const context = useContext(CityDataContext);
+	if (!context) {
+		throw new Error("useCityData must be used within a CityDataProvider");
+	}
+	return context;
 };
 
-export const CityDataProvider = ({ children }: CityDataProviderProps): JSX.Element => {
-  const [randomCity, setRandomCity] = useState<City | null>(null);
-  const [jsonData, setJsonData] = useState<any[]>([]);
-  const [cityDataMap, setCityDataMap] = useState<Map<string, CityAdditionalData> | null>(null);
+export const CityDataProvider = ({
+	children,
+}: CityDataProviderProps): JSX.Element => {
+	const [randomCity, setRandomCity] = useState<City | null>(null);
+	const [jsonData, setJsonData] = useState<any[]>([]);
+	const [cityDataMap, setCityDataMap] = useState<Map<
+		string,
+		CityAdditionalData
+	> | null>(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const prefsResponse = await fetch("/prefsSousPrefs.json");
-        if (!prefsResponse.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const prefsData = await prefsResponse.json();
-        setJsonData(prefsData);
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const prefsResponse = await fetch("/prefsSousPrefs.json");
+				if (!prefsResponse.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const prefsData = await prefsResponse.json();
+				setJsonData(prefsData);
 
-        const citiesResponse = await fetch("/cities.json");
-        if (!citiesResponse.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const citiesData = await citiesResponse.json();
-        const cityMap: Map<string, CityAdditionalData> = new Map(
-          citiesData.cities.map((city: CityAdditionalData) => [city.insee_code, city])
-        );
-        setCityDataMap(cityMap);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
+				const citiesResponse = await fetch("/cities.json");
+				if (!citiesResponse.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const citiesData = await citiesResponse.json();
+				const cityMap: Map<string, CityAdditionalData> = new Map(
+					citiesData.cities.map((city: CityAdditionalData) => [
+						city.insee_code,
+						city,
+					])
+				);
+				setCityDataMap(cityMap);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		}
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  useEffect(() => {
-    if (jsonData.length > 0 && cityDataMap && cityDataMap.size > 0) {
-      generateRandomCity();
-    }
-  }, [jsonData, cityDataMap]);
+	useEffect(() => {
+		if (jsonData.length > 0 && cityDataMap && cityDataMap.size > 0) {
+			generateRandomCity();
+		}
+	}, [jsonData, cityDataMap]);
 
-  const generateRandomCity = () => {
-    if (jsonData.length > 0 && cityDataMap && cityDataMap.size > 0) {
-      const randomIndex = Math.floor(Math.random() * jsonData.length);
-      const randomCityData = jsonData[randomIndex];
-      const additionalData = cityDataMap.get(randomCityData.insee_commune);
-      const nom_commune_without_accents = removeAccents(randomCityData.nom_commune);
-      setRandomCity({ ...randomCityData, additionalData, nom_commune: nom_commune_without_accents });
-      console.log("1", { ...randomCityData, additionalData });
-    }
-  };
+	const generateRandomCity = () => {
+		if (jsonData.length > 0 && cityDataMap && cityDataMap.size > 0) {
+			const randomIndex = Math.floor(Math.random() * jsonData.length);
+			const randomCityData = jsonData[randomIndex];
+			const additionalData = cityDataMap.get(randomCityData.insee_commune);
+			const nom_commune_without_accents = removeAccents(
+				randomCityData.nom_commune
+			);
+			setRandomCity({
+				...randomCityData,
+				additionalData,
+				nom_commune: nom_commune_without_accents,
+			});
+		}
+	};
 
-  const removeAccents = (str: string): string => {
-    if (!str) return "";
-    return str
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replace(/ç/g, "c")
-      .replace(/Ç/g, "C")
-      .replace(/é/g, "e")
-      .replace(/È/g, "E")
-  };
+	const removeAccents = (str: string): string => {
+		if (!str) return "";
+		return str
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, "")
+			.replace(/ç/g, "c")
+			.replace(/Ç/g, "C")
+			.replace(/é/g, "e")
+			.replace(/È/g, "E");
+	};
 
-  const contextValue: CityDataContextType = {
-    randomCity,
-    cityDataMap,
-    jsonData,
-    generateRandomCity
-  };
+	const contextValue: CityDataContextType = {
+		randomCity,
+		cityDataMap,
+		jsonData,
+		generateRandomCity,
+	};
 
-  return (
-    <CityDataContext.Provider value={contextValue}>
-      {children}
-    </CityDataContext.Provider>
-  );
+	return (
+		<CityDataContext.Provider value={contextValue}>
+			{children}
+		</CityDataContext.Provider>
+	);
 };
