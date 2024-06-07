a7589e1fe2022ef8a1dfd846c06218891026ce04
Félix SCHERER <felix.scherer@etu.umontpellier.fr>
2024-06-07 14:04:55 +0200

    style: loader 50vh

--- geodle/app/components/Layout/loader.tsx
+++ geodle/app/components/Layout/loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
 	return (
 		<>
-			<div className="flex justify-center items-center h-screen w-screen">
+			<div className="flex justify-center items-center h-[50vh] w-[50vh]">
 				<span className="loading loading-spinner loading-lg"></span>
 			</div>
 		</>
