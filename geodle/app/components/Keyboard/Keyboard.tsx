3fc6892a945b3376ea54022c8a709eb887d5f694
Félix SCHERER <felix.scherer@etu.umontpellier.fr>
2024-06-18 14:33:34 +0200

    fix: reset button taking control over Enter button

--- geodle/app/components/Keyboard/Keyboard.tsx
+++ geodle/app/components/Keyboard/Keyboard.tsx
@@ -11,131 +11,137 @@ const secondRow = ["Q", "S", "D", "F", "G", "H", "J", "K", "L", "M"];
 const thirdRow = ["W", "X", "C", "V", "B", "N", "-", "'"];
 
 type Props = {
-  onLetterClick: (letter: string) => void;
-  onSubmit: () => void;
-  onDelete: () => void;
-  onReset: () => void;
-  rows: Row[];
+    onLetterClick: (letter: string) => void;
+    onSubmit: () => void;
+    onDelete: () => void;
+    onReset: () => void;
+    rows: Row[];
 };
 
 export default function Keyboard({
-  onLetterClick,
-  onSubmit,
-  rows,
-  onDelete,
-  onReset,
+    onLetterClick,
+    onSubmit,
+    rows,
+    onDelete,
+    onReset,
 }: Props) {
-  const cells = useMemo(() => rows.flat(), [rows]);
+    const cells = useMemo(() => rows.flat(), [rows]);
 
-  const checkStatus = (key: string) => {
-    const cell = cells.find(
-      (cell) => cell.value === key && cell.status === "absent"
-    );
-    return cell ? "absent" : "guessing";
-  };
-
-  useEffect(() => {
-    function listener(e: KeyboardEvent) {
-      if (e.code === "Enter") {
-        onSubmit();
-      } else if (e.code === "Backspace") {
-        onDelete();
-      }
-      const key = e.key.toUpperCase();
-      if (
-        key.length === 1 &&
-        ((key >= "A" && key <= "Z") || key === "-" || key === "'")
-      ) {
-        onLetterClick(key);
-      }
-    }
-
-    function spaceKeyListener(e: KeyboardEvent) {
-      if (e.code === "Space") {
-        e.preventDefault();
-        onLetterClick(" ");
-      }
-    }
-
-    window.addEventListener("keyup", listener);
-    window.addEventListener("keydown", spaceKeyListener);
-
-    return () => {
-      window.removeEventListener("keyup", listener);
-      window.removeEventListener("keydown", spaceKeyListener);
+    const checkStatus = (key: string) => {
+        const cell = cells.find(
+            (cell) => cell.value === key && cell.status === "absent"
+        );
+        return cell ? "absent" : "guessing";
     };
-  }, [onLetterClick, onSubmit, onDelete]);
 
-	return (
-		<div className="space-y-2">
-			<div className="flex items-center justify-center space-x-2">
-				{firstRow.map((key) => (
-					<Key
-						key={key}
-						value={key}
-						onLetterClick={(letter) => onLetterClick(letter)}
-						status={checkStatus(key)}
-						type="keyboard"
-					/>
-				))}
-			</div>
-			<div className="flex items-center justify-center space-x-2">
-				{secondRow.map((key) => (
-					<Key
-						key={key}
-						value={key}
-						onLetterClick={(letter) => onLetterClick(letter)}
-						status={checkStatus(key)}
-						type="keyboard"
-					/>
-				))}
-			</div>
-			<div className="flex items-center justify-center  space-x-2">
-				{thirdRow.map((key) => (
-					<Key
-						key={key}
-						value={key}
-						onLetterClick={(letter) => onLetterClick(letter)}
-						status={checkStatus(key)}
-						type="keyboard"
-					/>
-				))}
-			</div>
-			<div className="flex items-center justify-center space-x-2">
-				<Key
-					key={" "}
-					value={" "}
-					onLetterClick={(letter) => onLetterClick(letter)}
-					status={checkStatus(" ")}
-					type="spacebar"
-				/>
-				<div className="tooltip" data-tip="Delete last input letter">
-					<button
-						className="border-2hidden lg:inline-flex items-center justify-center w-12 h-12 bg-red-500 border-red-500 text-white rounded"
-						onClick={onDelete}
-					>
-						<div className="flex items-center justify-center">
-							<FontAwesomeIcon icon={faDeleteLeft} />
-						</div>
-					</button>
-				</div>
-				<div className="tooltip" data-tip="Submit your word">
-					<button
-						className="border-2 hidden lg:inline-flex items-center justify-center w-12 h-12 bg-green-500 border-green-500 text-white rounded"
-						onClick={onSubmit}
-					>
-						<FontAwesomeIcon icon={faPaperPlane} />
-					</button>
-				</div>
-				<div className="tooltip" data-tip="Pick a new city">
-					<button
-						className="border-2	 hidden lg:inline-flex items-center justify-center w-12 h-12 bg-orange-500 border-orange-500 text-white rounded"
-						onClick={onReset}
-					>
-						<FontAwesomeIcon icon={faArrowsRotate} />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        function listener(e: KeyboardEvent) {
+            if (e.code === "Enter") {
+                onSubmit();
+            } else if (e.code === "Backspace") {
+                onDelete();
+            }
+            const key = e.key.toUpperCase();
+            if (
+                key.length === 1 &&
+                ((key >= "A" && key <= "Z") || key === "-" || key === "'")
+            ) {
+                onLetterClick(key);
+            }
+        }
+
+        function spaceKeyListener(e: KeyboardEvent) {
+            if (e.code === "Space") {
+                e.preventDefault();
+                onLetterClick(" ");
+            }
+        }
+
+        window.addEventListener("keyup", listener);
+        window.addEventListener("keydown", spaceKeyListener);
+
+        return () => {
+            window.removeEventListener("keyup", listener);
+            window.removeEventListener("keydown", spaceKeyListener);
+        };
+    }, [onLetterClick, onSubmit, onDelete]);
+
+    function handleReset(e: React.MouseEvent<HTMLButtonElement>): void {
+        e.preventDefault();
+        e.currentTarget.blur(); // Remove focus from the button
+        onReset();
+    }
+
+    return (
+        <div className="space-y-2">
+            <div className="flex items-center justify-center space-x-2">
+                {firstRow.map((key) => (
+                    <Key
+                        key={key}
+                        value={key}
+                        onLetterClick={(letter) => onLetterClick(letter)}
+                        status={checkStatus(key)}
+                        type="keyboard"
+                    />
+                ))}
+            </div>
+            <div className="flex items-center justify-center space-x-2">
+                {secondRow.map((key) => (
+                    <Key
+                        key={key}
+                        value={key}
+                        onLetterClick={(letter) => onLetterClick(letter)}
+                        status={checkStatus(key)}
+                        type="keyboard"
+                    />
+                ))}
+            </div>
+            <div className="flex items-center justify-center space-x-2">
+                {thirdRow.map((key) => (
+                    <Key
+                        key={key}
+                        value={key}
+                        onLetterClick={(letter) => onLetterClick(letter)}
+                        status={checkStatus(key)}
+                        type="keyboard"
+                    />
+                ))}
+            </div>
+            <div className="flex items-center justify-center space-x-2">
+                <Key
+                    key={" "}
+                    value={" "}
+                    onLetterClick={(letter) => onLetterClick(letter)}
+                    status={checkStatus(" ")}
+                    type="spacebar"
+                />
+                <div className="tooltip" data-tip="Delete last input letter">
+                    <button
+                        className="border-2 hidden lg:inline-flex items-center justify-center w-12 h-12 bg-red-500 border-red-500 text-white rounded"
+                        onClick={onDelete}
+                    >
+                        <div className="flex items-center justify-center">
+                            <FontAwesomeIcon icon={faDeleteLeft} />
+                        </div>
+                    </button>
+                </div>
+                <div className="tooltip" data-tip="Submit your word">
+                    <button
+                        className="border-2 hidden lg:inline-flex items-center justify-center w-12 h-12 bg-green-500 border-green-500 text-white rounded"
+                        onClick={onSubmit}
+                    >
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                </div>
+                <div className="tooltip" data-tip="Pick a new city">
+                    <button
+                        className="border-2 hidden lg:inline-flex items-center justify-center w-12 h-12 bg-orange-500 border-orange-500 text-white rounded"
+                        onClick={handleReset}
+                    >
+                        <FontAwesomeIcon icon={faArrowsRotate} />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
